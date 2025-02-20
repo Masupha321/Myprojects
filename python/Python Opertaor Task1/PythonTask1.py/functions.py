51dbe3c5ee727d6110be1298919361c52c6068ef
Masupha321 <masuphamojela33@gmail.com>
2025-02-20 14:07:42 +0100

    update files in the directory

---
+++ python/Python Opertaor Task1/PythonTask1.py/functions.py
@@ -0,0 +1,16 @@
+def calculate_average(num1, num2, num3, num4):
+    return (num1 + num2 + num3 + num4) / 4
+def main():
+    try:
+        num1 = float(input("Enter the first number"))
+        num2 = float(input("Enter the second number"))
+        num3 = float(input("Enter the third number"))
+        num4 = float(input("Enter the fourth number"))
+
+        average = calculate_average(num1, num2, num3, num4)
+        print(f"The average of the numbers is:, {average}")
+    except ValueError:
+        print("Please enter a valid numbers.")
+
+    if __name__ == "__main__":
+        main()
--- python/Python Opertaor Task1/PythonTask1.py/loops1.py
+++ python/Python Opertaor Task1/PythonTask1.py/loops1.py
@@ -2,5 +2,19 @@ def print_even_numbers():
     for num in range(1, 51):
         if num % 2 == 0:
             print(num)
-            
+
+
+
+def find_largest(numbers):
+    largest = numbers[0]
+    for num in numbers:
+        if num > largest:
+            largest = num
+    return largest
+
+if __name__ == '__main__':
+    numbers = [10, 25, 47, 89, 55, 102, 76]
+    print("The largest number is:", find_largest(numbers))
+
+
             
