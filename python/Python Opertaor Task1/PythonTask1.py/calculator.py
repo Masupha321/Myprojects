a7824b9648271b5b9b71c80556b2dc9985e126c2
Masupha321 <masuphamojela33@gmail.com>
2025-03-06 10:50:32 +0100

    update files in the directory

--- python/Python Opertaor Task1/PythonTask1.py/calculator.py
+++ python/Python Opertaor Task1/PythonTask1.py/calculator.py
@@ -11,7 +11,7 @@ def divide(a, b):
         return "Error: division by zero is not allowed"
     return a / b
 
-def calculator():
+def calculator(): #function definition
     while True:
         print("\nSimple Calculator")
         print("1. Addition")
@@ -21,3 +21,31 @@ def calculator():
         print("5. Exit")
 
         choice = int(input("\nEnter your choice (1-5): "))
+
+        match choice:
+            case 1:
+                num1= float(input("Enter first number: "))
+                num2= float(input("Enter second number: "))
+                print(f"Result: {add(num1, num2)}") #function call inside function definition   
+            case 2:
+                num1= float(input("Enter first number: "))
+                num2= float(input("Enter second number: "))
+                print(f"Result: {subtract(num1, num2)}") #function call inside function definition
+            case 3:
+                num1= float(input("Enter first number: "))
+                num2= float(input("Enter second number: "))
+                print(f"Result {multiply(num1, num2)}") #function call inside function definition
+            case 4:
+                num1= float(input("Enter first number: "))
+                num2=float(input("Enter second number: "))
+                print(f"Result {divide(num1, num2)}") #function call inside function definition
+            case 5:
+                print("Exiting...Goodbye!")
+                break
+            case _:
+                print("Invalid choice. Please try between 1 and 5.")
+
+calculator() #function call
+
+            
+
